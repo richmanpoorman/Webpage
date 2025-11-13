a21cfdfc73e3849e9b602b40443315e2379eb29e
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2025-11-13 00:10:27 -0500

    adding title web page

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,11 +1,17 @@
-import type { ReactElement } from 'react';
+import { useEffect, type ReactElement } from 'react';
 import SiteGrid from './components/SiteGrid/SiteGrid';
 // import { useState } from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 
+const title : string = "Matthew Wong"
+
 function App() : ReactElement {
+    useEffect(() => {
+        document.title = title;
+    });
+
     return (
         <SiteGrid/>
     );
